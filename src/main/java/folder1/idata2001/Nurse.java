6c68dbad93eda5e27bc78fb64b7ce11e7d31443d
Torstein <taurstein@gmail.com>
2021-03-02 16:26:05 +0100

    Added inheritence between class Nurse and class Employee

--- src/main/java/folder1/idata2001/Nurse.java
+++ src/main/java/folder1/idata2001/Nurse.java
@@ -3,17 +3,18 @@ package folder1.idata2001;
 /**
  * This class represents a real life nurse at a real life hospital
  */
-public class Nurse {
+public class Nurse extends Employee
+{
 
     /**
      * Constructor initializes an object of class Nurse
-     * @param firstName
-     * @param lastName
-     * @param socialSecurityNumber
+     * @param firstName first name of Nurse
+     * @param lastName last name of Nurse
+     * @param socialSecurityNumber Social security number of Nurse
      */
     public Nurse(String firstName, String lastName, String socialSecurityNumber)
     {
-
+        super(firstName, lastName, socialSecurityNumber);
     }
 
 }
